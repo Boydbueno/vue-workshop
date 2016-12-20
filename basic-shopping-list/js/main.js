47da7f0b8583e6f70d5914ca9ff6acf73dd55645
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2016-12-20 20:07:30 +0100

    Added basic javascript shopping list

---
+++ basic-shopping-list/js/main.js
@@ -0,0 +1,21 @@
+(function(){
+    document.getElementById("add-todo-form").addEventListener("submit", todoFormSubmitHandler);
+
+    function todoFormSubmitHandler(e) {
+        e.preventDefault();
+
+
+        var todoList = document.getElementById("todo-list");
+        var itemEl = document.getElementById("item");
+        var item = itemEl.value;
+        var li = document.createElement("li");
+        li.innerHTML = item;
+        li.className = "todo-item";
+        li.addEventListener("click", e => todoList.removeChild(e.target));
+
+        todoList.appendChild(li);
+
+        itemEl.value = "";
+        itemEl.focus();
+    }
+})();
