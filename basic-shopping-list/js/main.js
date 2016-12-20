3b5c9bad08ac81095ceeeb1905ef78f83b0d10e8
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2016-12-20 20:13:26 +0100

    Simplified some stuff

--- basic-shopping-list/js/main.js
+++ basic-shopping-list/js/main.js
@@ -1,21 +1,19 @@
-(function(){
-    document.getElementById("add-todo-form").addEventListener("submit", todoFormSubmitHandler);
+document.getElementById("add-todo-form").addEventListener("submit", todoFormSubmitHandler);
 
-    function todoFormSubmitHandler(e) {
-        e.preventDefault();
+function todoFormSubmitHandler(e) {
+    e.preventDefault();
 
 
-        var todoList = document.getElementById("todo-list");
-        var itemEl = document.getElementById("item");
-        var item = itemEl.value;
-        var li = document.createElement("li");
-        li.innerHTML = item;
-        li.className = "todo-item";
-        li.addEventListener("click", e => todoList.removeChild(e.target));
+    var todoList = document.getElementById("todo-list");
+    var itemEl = document.getElementById("item");
+    var item = itemEl.value;
+    var li = document.createElement("li");
+    li.innerHTML = item;
+    li.className = "todo-item";
+    li.addEventListener("click", e => todoList.removeChild(e.target));
 
-        todoList.appendChild(li);
+    todoList.appendChild(li);
 
-        itemEl.value = "";
-        itemEl.focus();
-    }
-})();
+    itemEl.value = "";
+    itemEl.focus();
+}
